9e115c8f33970ccf39769a421ea657f286dda49e
HammaadDarries <hamdar786@gmail.com>
2023-05-16 19:49:10 +0200

    Update sac_agent_node.py

--- sac_agent_pkg/sac_agent_node.py
+++ sac_agent_pkg/sac_agent_node.py
@@ -16,7 +16,9 @@ import torch
 
 MAX_SPEED = 2
 MAX_STEER = 0.4
-NOISE_FACTOR = 0.002
+NOISE_FACTOR = 0.0
+NUM_SCANS = 2
+NUM_LASERS = 20
 
 class SACAgent:
     def __init__(self, filepath):
@@ -38,8 +40,8 @@ class SACAgentNode (Node):
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.start_time = None
         nn_path = '/home/hammaad/sim_ws/src/sac_agent_pkg/Data/Main_Agent/gamma_0.99.pth'
-        self.scan_buffer = np.zeros((2, 20))
-        self.lidar_state = np.zeros(40)
+        self.scan_buffer = np.zeros((NUM_SCANS, NUM_LASERS))
+        self.lidar_state = np.zeros(NUM_SCANS * NUM_LASERS)
         self.lap_complete = False
         self.start_position = None
 
@@ -56,6 +58,8 @@ class SACAgentNode (Node):
         # Normalize ranges
         scaled_ranges = ranges/30
         # get 20 beams out of the 1080 by averaging every 54 beams
+        # 30 beams would be 36
+        # 10 beams is 108
         ranges = np.clip(scaled_ranges, 0, 1)
         mean_ranges = np.mean(ranges.reshape(-1, 54), axis=1)
         
@@ -63,21 +67,21 @@ class SACAgentNode (Node):
         # mean_ranges = np.mean(ranges[:-1].reshape(-1, 54), axis=1)
         scan = mean_ranges
 
-        if self.scan_buffer.all() ==0: # first reading
-            for i in range(2):
+        if self.scan_buffer.all() == 0: # first reading
+            for i in range(NUM_SCANS):
                 self.scan_buffer[i, :] = scan 
         else:
             self.scan_buffer = np.roll(self.scan_buffer, 1, axis=0)
             self.scan_buffer[0, :] = scan
 
-        lidar_state = np.reshape(self.scan_buffer, (20 * 2))
+        lidar_state = np.reshape(self.scan_buffer, (NUM_LASERS * NUM_SCANS))
 
         self.lidar_state = lidar_state
 
     def timer_callback(self):
         msg = AckermannDriveStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = 'Attempt_1_Lesgooo'
+        msg.header.frame_id = 'timer_callback'
 
         nn_state = self.lidar_state
         nn_action = self.agent.act(nn_state)
@@ -120,36 +124,6 @@ class SACAgentNode (Node):
         noise = np.random.normal(0, noise_factor, ranges.shape)
         noisy_ranges = ranges + noise
         return noisy_ranges
-
-    # def save_data_to_file(self):
-    #     with open('position_history.csv', 'w', newline='') as position_file, open('speed_history.csv', 'w', newline='') as speed_file:
-    #         position_writer = csv.writer(position_file)
-    #         speed_writer = csv.writer(speed_file)
-        
-    #         position_writer.writerow(['x', 'y'])
-    #         speed_writer.writerow(['speed'])
-
-    #         for pos, speed in zip(self.position_history, self.speed_history):
-    #             position_writer.writerow(pos)
-    #             speed_writer.writerow([speed])
-
-    # def save_data_to_file(self):
-    #     folder_name = f"PLOTS/speed_{MAX_SPEED}_{NOISE_FACTOR}_data"
-    #     os.makedirs(folder_name, exist_ok=True)
-
-    #     position_file_path = os.path.join(folder_name, 'position_history.csv')
-    #     speed_file_path = os.path.join(folder_name, 'speed_history.csv')
-
-    #     with open(position_file_path, 'w', newline='') as position_file, open(speed_file_path, 'w', newline='') as speed_file:
-    #         position_writer = csv.writer(position_file)
-    #         speed_writer = csv.writer(speed_file)
-
-    #         position_writer.writerow(['x', 'y'])
-    #         speed_writer.writerow(['speed'])
-
-    #         for pos, speed in zip(self.position_history, self.speed_history):
-    #             position_writer.writerow(pos)
-    #             speed_writer.writerow([speed])
 
     def save_data_to_file(self):
         folder_name = f"PLOTS/speed_{MAX_SPEED}_{NOISE_FACTOR}_data"
